5d06d427695f4f3e9dd406a26a99449bc0c3484e
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-07-12 20:14:32 +0530

    Reducer is ready to implemetnt

---
+++ src/redux/reducers/todo-reducer.js
@@ -0,0 +1,26 @@
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from "../actions/action-types";
+
+const initialState = [];
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            //add todo
+            return [...state, action.payload];
+        case DELETE_TODO:
+            //delete todo
+            const newState = state.filter((todo) => todo.id !== action.payload)
+            return newState;
+        case UPDATE_TODO:
+            //update todo
+            const updatedState = state.map(todo => {
+                if (todo.id == action.payload.todoId) {
+                    todo.title = action.payload.todo.title;
+                    todo.description = action.payload.todo.description;
+                }
+                return todo;
+            })
+            return updatedState;
+        default:
+            return state
+    }
+}
